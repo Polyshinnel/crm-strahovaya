84745f20f1c2a2cdb87b2c6df2ff531bb512b1c6
Nesterov Andrey <polyshinnel@gmail.com>
2021-09-03 16:51:46 +0300

    Добавил БД, и начал разрабатывать структуру

---
+++ _connectDB.php
@@ -0,0 +1,7 @@
+<?php
+
+$host='localhost';
+$db='basename';
+$user='username';
+$password='password';
+$charset='utf8';
---
+++ crm.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Сен 03 2021 г., 16:50
+-- Версия сервера: 5.6.38
+-- Версия PHP: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `crm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `deals`
+--
+
+CREATE TABLE `deals` (
+  `id` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `type` varchar(256) NOT NULL,
+  `name_applicant` varchar(256) NOT NULL,
+  `name_organization` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `stages`
+--
+
+CREATE TABLE `stages` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `color` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `deals`
+--
+ALTER TABLE `deals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `stages`
+--
+ALTER TABLE `stages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `deals`
+--
+ALTER TABLE `deals`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `stages`
+--
+ALTER TABLE `stages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
